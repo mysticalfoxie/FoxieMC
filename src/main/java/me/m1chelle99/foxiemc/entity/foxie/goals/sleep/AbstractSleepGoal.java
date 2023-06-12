23229cdebacba2895f6dd6e4f5357c1362357a12
Michelle-99 <MichelleBecker2003@gmail.com>
2023-06-12 19:53:36 +0200

    Fix some codacy issues

--- src/main/java/me/m1chelle99/foxiemc/entity/foxie/goals/sleep/AbstractSleepGoal.java
+++ src/main/java/me/m1chelle99/foxiemc/entity/foxie/goals/sleep/AbstractSleepGoal.java
@@ -6,33 +6,33 @@ import net.minecraft.world.entity.ai.goal.Goal;
 
 // More than this dev sleeps on a normal work day uwu
 public class AbstractSleepGoal extends Goal {
-	protected final Foxie foxie;
+    protected final Foxie foxie;
 
-	public AbstractSleepGoal(Foxie foxie) {
-		this.foxie = foxie;
-	}
+    public AbstractSleepGoal(Foxie foxie) {
+        this.foxie = foxie;
+    }
 
-	@Override
-	public boolean canUse() {
-		if (this.foxie.aiControl.isSleeping()) return false;
-		return this.foxie.aiControl.canSleep();
-	}
+    @Override
+    public boolean canUse() {
+        if (this.foxie.aiControl.isSleeping()) return false;
+        return this.foxie.aiControl.canSleep();
+    }
 
-	public boolean canContinueToUse() {
-		return this.foxie.aiControl.canSleep();
-	}
+    public boolean canContinueToUse() {
+        return this.foxie.aiControl.canSleep();
+    }
 
-	@Override
-	public void start() {
-		this.foxie.aiControl.startActivity(FoxieConstants.ACTIVITY_SLEEP);
-		var position = this.foxie.blockPosition();
-		this.foxie.startSleeping(position);
-	}
+    @Override
+    public void start() {
+        this.foxie.aiControl.startActivity(FoxieConstants.ACTIVITY_SLEEP);
+        var position = this.foxie.blockPosition();
+        this.foxie.startSleeping(position);
+    }
 
-	@Override
-	public void stop() {
-		this.foxie.stopSleeping();
-		if (this.foxie.aiControl.hasActivity(FoxieConstants.ACTIVITY_SLEEP))
-			this.foxie.aiControl.startActivity(FoxieConstants.ACTIVITY_NONE);
-	}
+    @Override
+    public void stop() {
+        this.foxie.stopSleeping();
+        if (this.foxie.aiControl.hasActivity(FoxieConstants.ACTIVITY_SLEEP))
+            this.foxie.aiControl.startActivity(FoxieConstants.ACTIVITY_NONE);
+    }
 }
